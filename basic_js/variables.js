4c2fa7c43528b9ebbbab768f04a6b25983d01300
bhattaraisamip <bhattaraisamip43@gmail.com>
2026-05-07 14:51:41 +0000

    confic done

--- basic_js/variables.js
+++ basic_js/variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail ="samip@gmail.com"
 var accountPw ="34543"
 accountCity ="kathmandu"
+let accountState;
 
 // accountId = 2 not allowed 
 
@@ -10,9 +11,13 @@ prefer not to use var
 because of issue in block scpe and functional scope
 */
 
+/*
+node basic_js/variables.js
+*/
+
 accountEmail="bhattarai@gmail.com"
 accountPw="46322"
 accountCity="pokhara"
 
 console.log(accountId);
-console.table([accountId ,accountEmail ,accountPw ,accountCity])
+console.table([accountId ,accountEmail ,accountPw ,accountCity ,accountState])
