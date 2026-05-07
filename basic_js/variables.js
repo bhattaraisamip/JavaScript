319c466a8c0c5789e832f9b197bd8c28d97e4f39
bhattaraisamip <bhattaraisamip43@gmail.com>
2026-05-07 14:47:45 +0000

    config done

---
+++ basic_js/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail ="samip@gmail.com"
+var accountPw ="34543"
+accountCity ="kathmandu"
+
+// accountId = 2 not allowed 
+
+/*
+prefer not to use var 
+because of issue in block scpe and functional scope
+*/
+
+accountEmail="bhattarai@gmail.com"
+accountPw="46322"
+accountCity="pokhara"
+
+console.log(accountId);
+console.table([accountId ,accountEmail ,accountPw ,accountCity])
