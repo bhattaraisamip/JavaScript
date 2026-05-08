c1e85cc8eb3ab1f654d123b8aee771a589e33ca7
bhattaraisamip <bhattaraisamip43@gmail.com>
2026-05-08 07:12:33 +0000

    config done

---
+++ basic_js/datatype.js
@@ -0,0 +1,33 @@
+"use strict";//treat all js code as newer version 
+
+//alert("hello") //we are using node js not browser 
+
+console.log(3
+    +
+    3) //Code readability should be high 
+
+console.log("samip");
+
+
+let name="samip"
+let age=20
+let isLoggedIn = false
+let state;
+
+// range of number => 2 to the power of 53
+// bigint 
+// string => ""
+//boolean => true/false
+// null => standAlone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);// object 
+console.log(typeof undefined);// undefinde  
+
+// node basic_js/datatype.js
