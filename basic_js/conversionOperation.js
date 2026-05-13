ae9b7568fd8e485bc4f770f383e49fead2066851
bhattaraisamip <bhattaraisamip43@gmail.com>
2026-05-13 14:10:09 +0000

    config

--- basic_js/conversionOperation.js
+++ basic_js/conversionOperation.js
@@ -1,4 +1,3 @@
- //node basic_js/conversionOperation.js
 let score = "samnip"
 
 // console.log(typeof score);
@@ -7,7 +6,6 @@ let score = "samnip"
  let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
-
 //"43" => 43
 //"43abc"=> NaN
 //true => 1 ; false =>0
@@ -24,5 +22,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 45
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************** OPERATIONS ********************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "samip "
+let str2 =" bhattarai"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+//node basic_js/conversionOperation.js
