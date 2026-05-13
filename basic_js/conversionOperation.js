444c318b1cad499d5630515a99f61834a9eeab1a
bhattaraisamip <bhattaraisamip43@gmail.com>
2026-05-13 13:17:43 +0000

    config done

---
+++ basic_js/conversionOperation.js
@@ -0,0 +1,22 @@
+ //node basic_js/conversionOperation.js
+//  let score = "samnip"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"43" => 43
+//"43abc"=> NaN
+//true => 1 ; false =>0
+
+let isLoggedIn = "samip"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"samip" => true (valueInsideTheStringIsTrue)
